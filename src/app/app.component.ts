5bdcf824603c3c01db0532db456a54be1d71de11
Henrique Couto <email@henriquecouto.com.br>
2020-10-20 10:09:00 -0300

    simple todo working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app-list';
+  taskName = '';
+  taskList = [];
+
+  addTask() {
+    if (!this.taskName) {
+      return;
+    }
+
+    this.taskList.push(this.taskName);
+    this.taskName = '';
+    console.log(this.taskList);
+  }
+
+  removeTask(index: number) {
+    this.taskList.splice(index, 1);
+  }
 }
