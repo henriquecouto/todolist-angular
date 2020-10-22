f94b2192ce8fb522122c0e5d7fd18795510246bd
Henrique Couto <email@henriquecouto.com.br>
2020-10-22 16:47:51 -0300

    validate create task form

--- src/app/components/create-task/create-task.component.ts
+++ src/app/components/create-task/create-task.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import Task from 'src/entities/Task';
 
 @Component({
@@ -11,13 +11,40 @@ export class CreateTaskComponent implements OnInit {
   @Input() create: (task: Task) => void;
   taskForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.taskForm = this.formBuilder.group({ taskName: '', projectName: '' });
+  taskValidationMessages = {
+    taskName: [
+      {
+        type: 'required',
+        message: 'Você precisa inserir um nome para a tarefa',
+      },
+    ],
+    projectName: [
+      { type: 'required', message: 'Você precisa inserir o nome do projeto' },
+    ],
+  };
+
+  constructor() {
+    this.taskForm = new FormGroup({
+      taskName: new FormControl('', Validators.required),
+      projectName: new FormControl('', Validators.required),
+    });
   }
 
   ngOnInit() {}
 
+  get taskName() {
+    return this.taskForm.get('taskName');
+  }
+
+  get projectName() {
+    return this.taskForm.get('projectName');
+  }
+
   onSubmitForm = ({ taskName, projectName }) => {
+    if (!this.taskForm.valid) {
+      return;
+    }
+
     this.create(new Task({ name: taskName, project: projectName }));
     this.taskForm.reset();
   };
