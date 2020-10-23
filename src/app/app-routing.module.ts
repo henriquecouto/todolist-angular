2a93c732801e5460376620e1db6117055c837f74
Henrique Couto <email@henriquecouto.com.br>
2020-10-23 12:03:58 -0300

    navigate to project details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { TaskAreaComponent } from './components/task-area/task-area.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskAreaComponent },
+  { path: 'project/:projectName', component: ProjectDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/projects.module.ts
+++ src/app/modules/projects.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsAreaComponent } from '../components/projects-area/projects-area.component';
+import { ProjectDetailsComponent } from '../components/project-details/project-details.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [ProjectsAreaComponent],
-  imports: [CommonModule],
+  declarations: [ProjectsAreaComponent, ProjectDetailsComponent],
+  imports: [CommonModule, AppRoutingModule],
   exports: [ProjectsAreaComponent],
 })
 export class ProjectsModule {}
