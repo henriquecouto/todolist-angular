3937411bc6be05d9c09fc06cc95ae563d260e821
Henrique Couto <email@henriquecouto.com.br>
2020-10-27 12:41:20 -0300

    ensure task list show all tasks

--- src/app/components/task-list/task-list.component.spec.ts
+++ src/app/components/task-list/task-list.component.spec.ts
@@ -1,6 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import Task from 'src/entities/Task';
 
 import { TaskListComponent } from './task-list.component';
+
+const mockTasks = [
+  new Task({ name: 'hehe', project: 'hehe' }),
+  new Task({ name: 'haha', project: 'haha' }),
+  new Task({ name: 'haha', project: 'haha' }),
+  new Task({ name: 'haha', project: 'haha' }),
+  new Task({ name: 'haha', project: 'haha' }),
+];
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
@@ -9,12 +21,27 @@ describe('TaskListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TaskListComponent],
+      imports: [CommonModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
+    component.tasks = mockTasks;
     fixture.detectChanges();
+  });
+
+  it('should create task list', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should tasks are showed', async () => {
+    const tasksLength = component.tasks.length;
+    const renderedTasksLength = fixture.debugElement.queryAll(
+      By.css('.task-item')
+    ).length;
+
+    expect(renderedTasksLength).toBe(tasksLength);
   });
 });
